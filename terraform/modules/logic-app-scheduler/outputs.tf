c0ffe96e4f0d2e8f149dc53e483990d96befcab3
Tom Boone <tbc@tomboone.com>
2025-10-20 15:02:49 -0400

    rebuild logic app to support authz

--- terraform/modules/logic-app-scheduler/outputs.tf
+++ terraform/modules/logic-app-scheduler/outputs.tf
@@ -2,20 +2,21 @@
 
 output "logic_app_id" {
   description = "ID of the Logic App"
-  value       = azurerm_logic_app_workflow.scheduler.id
+  value       = data.azurerm_logic_app_workflow.scheduler.id
 }
 
 output "logic_app_name" {
   description = "Name of the Logic App"
-  value       = azurerm_logic_app_workflow.scheduler.name
+  value       = data.azurerm_logic_app_workflow.scheduler.name
 }
 
 output "logic_app_identity_principal_id" {
   description = "Principal ID of the Logic App managed identity"
-  value       = azurerm_logic_app_workflow.scheduler.identity[0].principal_id
+  # IDE warning about "Unresolved reference" is expected - the output structure is defined in the ARM template
+  value       = jsondecode(azurerm_resource_group_template_deployment.scheduler.output_content).principalId.value
 }
 
 output "logic_app_access_endpoint" {
   description = "Access endpoint of the Logic App"
-  value       = azurerm_logic_app_workflow.scheduler.access_endpoint
+  value       = data.azurerm_logic_app_workflow.scheduler.access_endpoint
 }
