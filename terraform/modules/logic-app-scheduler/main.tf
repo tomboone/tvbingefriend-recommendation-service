a0b77705a46411ebf17a827da87312680761e8e5
Tom Boone <tbc@tomboone.com>
2025-10-20 15:02:49 -0400

    rebuild logic app to support authz

--- terraform/modules/logic-app-scheduler/main.tf
+++ terraform/modules/logic-app-scheduler/main.tf
@@ -1,54 +1,28 @@
 # Logic App for scheduled container execution
 # This creates a Logic App that runs the recommendation pipeline on a schedule
+# Using ARM template to support managed identity authentication for HTTP actions
 
 locals {
   # Parse schedule time (e.g., "02:00" -> hour=2, minute=0)
   schedule_parts = split(":", var.schedule_time)
   schedule_hour  = local.schedule_parts[0]
   schedule_minute = local.schedule_parts[1]
-}
 
-resource "azurerm_logic_app_workflow" "scheduler" {
-  name                = var.logic_app_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    purpose = "recommendation-pipeline-scheduler"
+  # Build schedule configuration for recurrence trigger
+  schedule_config = var.schedule_frequency == "Week" ? {
+    hours   = [tonumber(local.schedule_hour)]
+    minutes = [tonumber(local.schedule_minute)]
+    weekDays = var.schedule_days_of_week
+  } : {
+    hours   = [tonumber(local.schedule_hour)]
+    minutes = [tonumber(local.schedule_minute)]
   }
 }
 
-# Role assignment to allow Logic App to manage Container Instances
-resource "azurerm_role_assignment" "container_contributor" {
-  scope                = var.container_group_id
-  role_definition_name = "Contributor"
-  principal_id         = azurerm_logic_app_workflow.scheduler.identity[0].principal_id
-}
-
-# Deploy the trigger
-resource "azurerm_logic_app_trigger_recurrence" "schedule" {
-  name         = "RecurrenceSchedule"
-  logic_app_id = azurerm_logic_app_workflow.scheduler.id
-  frequency    = var.schedule_frequency
-  interval     = var.schedule_interval
-  time_zone    = "UTC"
-
-  schedule {
-    at_these_hours   = [tonumber(local.schedule_hour)]
-    at_these_minutes = [tonumber(local.schedule_minute)]
-    on_these_days    = var.schedule_frequency == "Week" ? var.schedule_days_of_week : []
-  }
-}
-
-# Deploy the action to start the container group
-# Note: Container will auto-execute pipeline on start, then stop due to OnFailure restart policy
-# Using ARM template because azurerm_logic_app_action_http doesn't support managed identity auth
-resource "azurerm_resource_group_template_deployment" "start_container_action" {
-  name                = "logic-app-http-action"
+# Deploy Logic App with complete workflow definition via ARM template
+# This is necessary to configure managed identity authentication for HTTP actions
+resource "azurerm_resource_group_template_deployment" "scheduler" {
+  name                = "logic-app-scheduler-deployment"
   resource_group_name = var.resource_group_name
   deployment_mode     = "Incremental"
 
@@ -57,27 +31,76 @@ resource "azurerm_resource_group_template_deployment" "start_container_action" {
     "contentVersion" : "1.0.0.0",
     "resources" : [
       {
-        "type" : "Microsoft.Logic/workflows/actions",
+        "type" : "Microsoft.Logic/workflows",
         "apiVersion" : "2019-05-01",
-        "name" : "${azurerm_logic_app_workflow.scheduler.name}/Start_Container_Group",
+        "name" : var.logic_app_name,
+        "location" : var.location,
+        "identity" : {
+          "type" : "SystemAssigned"
+        },
+        "tags" : {
+          "purpose" : "recommendation-pipeline-scheduler"
+        },
         "properties" : {
-          "type" : "Http",
-          "inputs" : {
-            "method" : "POST",
-            "uri" : "https://management.azure.com${var.container_group_id}/start?api-version=2021-09-01",
-            "authentication" : {
-              "type" : "ManagedServiceIdentity",
-              "audience" : "https://management.azure.com/"
+          "definition" : {
+            "$schema" : "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#",
+            "contentVersion" : "1.0.0.0",
+            "triggers" : {
+              "RecurrenceSchedule" : {
+                "type" : "Recurrence",
+                "recurrence" : {
+                  "frequency" : var.schedule_frequency,
+                  "interval" : var.schedule_interval,
+                  "timeZone" : "UTC",
+                  "schedule" : local.schedule_config
+                }
+              }
+            },
+            "actions" : {
+              "Start_Container_Group" : {
+                "type" : "Http",
+                "inputs" : {
+                  "method" : "POST",
+                  "uri" : "https://management.azure.com${var.container_group_id}/start?api-version=2021-09-01",
+                  "authentication" : {
+                    "type" : "ManagedServiceIdentity",
+                    "audience" : "https://management.azure.com/"
+                  }
+                },
+                "runAfter" : {}
+              }
             }
-          },
-          "runAfter" : {}
+          }
         }
       }
-    ]
+    ],
+    "outputs" : {
+      "logicAppId" : {
+        "type" : "string",
+        "value" : "[resourceId('Microsoft.Logic/workflows', '${var.logic_app_name}')]"
+      },
+      "principalId" : {
+        "type" : "string",
+        "value" : "[reference(resourceId('Microsoft.Logic/workflows', '${var.logic_app_name}'), '2019-05-01', 'Full').identity.principalId]"
+      }
+    }
   })
+}
 
-  depends_on = [
-    azurerm_logic_app_trigger_recurrence.schedule,
-    azurerm_role_assignment.container_contributor
-  ]
+# Get the Logic App details from the deployment outputs
+data "azurerm_logic_app_workflow" "scheduler" {
+  name                = var.logic_app_name
+  resource_group_name = var.resource_group_name
+
+  depends_on = [azurerm_resource_group_template_deployment.scheduler]
+}
+
+# Role assignment to allow Logic App to manage Container Instances
+resource "azurerm_role_assignment" "container_contributor" {
+  scope                = var.container_group_id
+  role_definition_name = "Contributor"
+  # Extract principal ID from ARM template deployment outputs
+  # IDE warning about "Unresolved reference" is expected - the output structure is defined in the ARM template
+  # noinspection HILUnresolvedReference
+  principal_id         = jsondecode(azurerm_resource_group_template_deployment.scheduler.output_content).principalId.value
 }
